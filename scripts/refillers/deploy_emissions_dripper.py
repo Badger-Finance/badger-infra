f483a73134d94317270e0164220935cd1a06768e
jorijnsmit <mail@gosuto.ai>
2022-06-13 16:27:09 +0200

    chore: automatically check for chain id

--- scripts/refillers/deploy_emissions_dripper.py
+++ scripts/refillers/deploy_emissions_dripper.py
@@ -1,7 +1,7 @@
 from  calendar import timegm
 from datetime import date, timedelta
 
-from brownie import EmissionsDripper, accounts, network
+from brownie import EmissionsDripper, accounts, chain, network
 
 from helpers.addresses import registry
 
@@ -9,28 +9,25 @@ from helpers.addresses import registry
 def main(deployer_label=None):
     deployer = accounts[0] if not deployer_label else accounts.load(deployer_label)
     on_live_network = not '-fork' in network.show_active()
-    return EmissionsDripper.deploy(
-        registry.eth.sett_vaults.remBADGER,
-        timegm(date(2022, 4, 29).timetuple()),
-        int(timedelta(weeks=9).total_seconds()),
-        registry.eth.badger_wallets.techops_multisig,
-        registry.eth.badger_wallets.dev_multisig,
-        deployer,
-        {'from': deployer},
-        publish_source=on_live_network
-    )
-
-
-def rinkeby(deployer_label=None):
-    deployer = accounts.load(deployer_label)
-    on_live_network = not '-fork' in network.show_active()
-    return EmissionsDripper.deploy(
-        registry.rinkeby.badger_wallets.solo_multisig,
-        timegm(date(2022, 4, 15).timetuple()),
-        int(timedelta(weeks=11).total_seconds()),
-        registry.rinkeby.badger_wallets.rinkeby_multisig,
-        registry.rinkeby.badger_wallets.rinkeby_multisig,
-        deployer,
-        {'from': deployer},
-        publish_source=on_live_network
-    )
+    if chain.id == 1:
+        return EmissionsDripper.deploy(
+            registry.eth.sett_vaults.remBADGER,
+            timegm(date(2022, 4, 29).timetuple()),
+            int(timedelta(weeks=9).total_seconds()),
+            registry.eth.badger_wallets.techops_multisig,
+            registry.eth.badger_wallets.dev_multisig,
+            deployer,
+            {'from': deployer},
+            publish_source=on_live_network
+        )
+    elif chain.id == 4:
+        return EmissionsDripper.deploy(
+            registry.rinkeby.badger_wallets.solo_multisig,
+            timegm(date(2022, 4, 15).timetuple()),
+            int(timedelta(weeks=11).total_seconds()),
+            registry.rinkeby.badger_wallets.rinkeby_multisig,
+            registry.rinkeby.badger_wallets.rinkeby_multisig,
+            deployer,
+            {'from': deployer},
+            publish_source=on_live_network
+        )
