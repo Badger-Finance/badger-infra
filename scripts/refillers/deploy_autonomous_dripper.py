7eff9be885b2a431f25f4fc7b2fcad2aee0b9976
gosuto-inzasheru <mail@gosuto.ai>
2022-12-19 14:58:20 +0530

    chore: update deploy script for 2023

--- scripts/refillers/deploy_autonomous_dripper.py
+++ scripts/refillers/deploy_autonomous_dripper.py
@@ -3,36 +3,37 @@ from datetime import date, timedelta
 
 from brownie import AutonomousDripper, accounts, chain, network
 
-from helpers.addresses import registry
+from helpers.addresses import r
 
 
 def main(deployer_label=None):
     deployer = accounts[0] if not deployer_label else accounts.load(deployer_label)
-    on_live_network = not '-fork' in network.show_active()
+    on_live_network = not "-fork" in network.show_active()
     if chain.id == 1:
         return AutonomousDripper.deploy(
-            registry.eth.badger_wallets.techops_multisig, # address initialOwner
-            registry.eth.badger_wallets.badgertree, # address beneficiaryAddress
-            timegm(date(2022, 7, 1).timetuple()), # uint64 startTimestamp
-            int(timedelta(weeks=13).total_seconds()), # uint64 durationSeconds
-            60*60*24*7, # uint intervalSeconds
-            [registry.eth.treasury_tokens.BADGER], # address[] memory watchlistAddresses
-            registry.eth.chainlink.keeper_registry, # address keeperRegistryAddress
-            {'from': deployer},
-            publish_source=on_live_network
+            r.badger_wallets.techops_multisig,  # address initialOwner
+            r.sett_vaults.remBADGER,  # address beneficiaryAddress
+            timegm(date(2023, 1, 1).timetuple()),  # uint64 startTimestamp
+            int(timedelta(year=1).total_seconds()),  # uint64 durationSeconds
+            60 * 60 * 24 * 7,  # uint intervalSeconds
+            [r.treasury_tokens.BADGER],  # address[] memory watchlistAddresses
+            r.chainlink.keeper_registry,  # address keeperRegistryAddress
+            {"from": deployer},
+            publish_source=on_live_network,
         )
-    elif chain.id == 4:
-        return AutonomousDripper.deploy(
-            deployer,
-            registry.rinkeby.badger_wallets.solo_multisig,
-            timegm(date.today().timetuple()),
-            int(timedelta(weeks=1).total_seconds()),
-            60*60, # interval of one hour
-            [
-                registry.rinkeby.treasury_tokens.DAI,
-                registry.rinkeby.treasury_tokens.WBTC,
-            ],
-            registry.rinkeby.chainlink.keeper_registry,
-            {'from': deployer},
-            publish_source=on_live_network
-        )
+    # TODO: rinkeby no longer supported, use goerli instead
+    # elif chain.id == 4:
+    #     return AutonomousDripper.deploy(
+    #         deployer,
+    #         registry.rinkeby.badger_wallets.solo_multisig,
+    #         timegm(date.today().timetuple()),
+    #         int(timedelta(weeks=1).total_seconds()),
+    #         60 * 60,  # interval of one hour
+    #         [
+    #             registry.rinkeby.treasury_tokens.DAI,
+    #             registry.rinkeby.treasury_tokens.WBTC,
+    #         ],
+    #         registry.rinkeby.chainlink.keeper_registry,
+    #         {"from": deployer},
+    #         publish_source=on_live_network,
+    #     )
